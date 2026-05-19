b6550104b96309a3a1ea484d78dae6d2bb2b1210
lemossco <contacto@lemoss.co>
2026-05-19 00:18:49 -0600

    fix: revert webpack alias hack, Node.js version en Vercel cambiado a 20.x

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "@prisma/client": false,
-        bcryptjs: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
