a2ce4625c5872517b638818477806ed5ba022775
lemossco <contacto@lemoss.co>
2026-05-19 00:06:04 -0600

    fix: alias @prisma/client=false en webpack client para resolver Module not found

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@prisma/client": false,
+        bcryptjs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
